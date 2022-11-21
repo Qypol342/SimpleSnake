2ed3e2de3a4e1359967092617e3b3e317d070021
MAEGHT LOAN p2103642 <p2103642@univ-lyon1.fr>
2022-11-21 09:08:03 +0100

    food added

--- SRC/JS/script.js
+++ SRC/JS/script.js
@@ -2,20 +2,35 @@ const canGame = document.querySelector("canvas")
 const ctx = canGame.getContext("2d")
 const startBtn = document.querySelector("#start");
 
-document.addEventListener("keybown",keyDown);
+document.addEventListener("keydown",keyDown);
 document.addEventListener("click",run)
 
-const tileCount=20;
-const tileSize=18;
-const MaxY = 400;
-const MaxX = 800;
+
+const MaxY = 600;
+const MaxX = 600;
 const speed = 7;
+const tileCount=20;
+const tileSize=MaxX/tileCount;
 
 let SnakeX =1;
 let SnakeY = 1;
 
 let VelX =1;
 let VelY=0;
+
+class Apple{
+    constructor(x, y){
+        this.x=x;
+        this.y=y;
+    }
+} 
+
+let lost = false
+let apples = [new Apple( 5,5)]
+let Score =0
+
+
+
 
 function clearScreen(){
     ctx.fillStyle="green"
@@ -24,15 +39,44 @@ function clearScreen(){
 
 function drawSnake(){
     ctx.fillStyle = "orange"
-    ctx.fillRect(1 *tileCount ,1 * tileCount,2 * tileSize,2 * tileSize)
+    ctx.fillRect(SnakeX *tileCount ,SnakeY * tileCount,2 * tileSize,2 * tileSize)
+}
+
+function drawFood(){
+    apples.forEach(e => {
+        ctx.fillStyle = "red"
+    ctx.fillRect(e.x *tileCount ,e.y * tileCount,2 * tileSize,2 * tileSize)
+        
+    });
+}
+
+function checkCollison(){
+    // food
+
+    apples.forEach(e => {
+        if (e.x == SnakeX && e.y == SnakeY ){
+            apples.pop(e)
+            console.log("food eated")
+            Score ++;
+            apples.push(new Apple(Math.floor(Math.random()*tileCount),Math.floor(Math.random()*tileCount)))
+        }
+        
+    });
+
+    //eadge
 }
 
 function run(){
-    console.log("runiong")
+    if (lost){ return }
+    
 
     SnakeX += VelX;
     SnakeY += VelY;
 
+    clearScreen();
+    checkCollison();
+    drawFood();
+    drawSnake();
     setTimeout(run, 1000/speed)
 }
 
@@ -44,23 +88,27 @@ drawSnake()
 
 
 function keyDown(event){
-
+    console.log("key down")
     if(event.keyCode==38){
+        if(VelY == 1){return}
         VelY=-1;
         VelX=0;
 
     }
 
     if(event.keyCode==40){
+        if(VelY == -1){return}
         VelY=1;
         VelX=0;
     }
 
     if(event.keyCode==37){
+        if(VelX == 1){return}
         VelY=0;
         VelX=-1;
     }
     if(event.keyCode==39){
+        if(VelX == -1){return}
         VelY=0;
         VelX=1;
     }
