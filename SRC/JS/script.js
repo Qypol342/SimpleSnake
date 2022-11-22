8fdfe7e687d6039caa4157ed819d24cd9fd65779
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2022-11-22 17:47:04 +0100

    Adding queue but not good following

--- SRC/JS/script.js
+++ SRC/JS/script.js
@@ -1,3 +1,17 @@
+class snakeParts {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Apple {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
 const canGame = document.querySelector("canvas");
 const ctx = canGame.getContext("2d");
 const startBtn = document.querySelector("#start");
@@ -17,22 +31,7 @@ let SnakeY = 1;
 let VelX = 1;
 let VelY = 0;
 
-const Parts = [];
-let snakeLenght = 2;
-
-class snakeParts {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-
-class Apple {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
+const Parts = [new snakeParts(SnakeX, SnakeY)];
 
 let lost = false;
 let apples = [new Apple(5, 5)];
@@ -55,13 +54,12 @@ function drawSnake() {
   for (let i = 0; i < Parts.length; i++) {
     let part = Parts[i];
     ctx.fillRect(
-      SnakeX * tileCount,
-      SnakeY * tileCount,
+      part.x * tileCount,
+      part.y * tileCount,
       2 * tileSize,
       2 * tileSize
     );
   }
-  Parts.push(new snakeParts(SnakeX, SnakeY));
 }
 
 function drawFood() {
@@ -71,14 +69,34 @@ function drawFood() {
   });
 }
 
+function addSnakePart() {
+  if (VelX == 1) {
+    Parts.push(
+      new snakeParts(Parts[Parts.length - 1].x - 1, Parts[Parts.length - 1].y)
+    );
+  } else if (VelX == -1) {
+    Parts.push(
+      new snakeParts(Parts[Parts.length - 1].x + 1, Parts[Parts.length - 1].y)
+    );
+  } else if (VelY == 1) {
+    Parts.push(
+      new snakeParts(Parts[Parts.length - 1].x, Parts[Parts.length - 1].y - 1)
+    );
+  } else if (VelY == -1) {
+    Parts.push(
+      new snakeParts(Parts[Parts.length - 1].x, Parts[Parts.length - 1].y + 1)
+    );
+  }
+}
 function checkCollison() {
   // food
 
   apples.forEach((e) => {
-    if (e.x == SnakeX && e.y == SnakeY) {
+    if (e.x == Parts[0].x && e.y == Parts[0].y) {
       apples.pop(e);
       console.log("food");
       score++;
+      addSnakePart();
       apples.push(
         new Apple(
           Math.floor(Math.random() * tileCount),
@@ -93,13 +111,20 @@ function checkCollison() {
 
 function checkOutside() {
   if (
-    SnakeX < 0 ||
-    SnakeX > tileCount + tileSize ||
-    SnakeY < 0 ||
-    SnakeY > tileCount + tileSize
+    Parts[0].x < 0 ||
+    Parts[0].x > tileCount + tileSize ||
+    Parts[0].y < 0 ||
+    Parts[0].y > tileCount + tileSize
   ) {
     console.log("out");
     return true;
+  }
+}
+
+function moveSnake() {
+  for (let i = 0; i < Parts.length; i++) {
+    Parts[i].x += VelX;
+    Parts[i].y += VelY;
   }
 }
 
@@ -107,20 +132,17 @@ function run() {
   if (lost) {
     location.reload();
   }
-
-  SnakeX += VelX;
-  SnakeY += VelY;
+  console.log(VelX, VelY);
+  moveSnake();
 
   clearScreen();
   checkCollison();
 
   lost = checkOutside();
-  console.log(lost);
   drawFood();
   drawSnake();
   drawScore();
-  console.log(snakeParts);
-  console.log(SnakeX, " ", SnakeY);
+  console.log(Parts);
   setTimeout(run, 1000 / speed);
 }
 
